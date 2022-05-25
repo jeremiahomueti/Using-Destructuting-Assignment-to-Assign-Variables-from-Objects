4f9ad81daa14cf5833288d43c196ff6d915ce0fa
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-25 18:01:57 +0100

    Update

---
+++ index.js
@@ -0,0 +1,16 @@
+//Use Destructuring Assignment to Assign Variables from Objects
+
+
+//The next few lessons will be about destructuring assignments.
+
+var voxel = {x: 3.6, y: 7.4, z: 6.54};
+
+
+//old way of destructuring each variable and assigning them to
+//..their own variables
+var x = voxel.x; //x = 3.6
+var y = voxel.y; //y = 7.4
+var z = voxel.z; //z = 6.54
+
+
+//to be continued in index1.js...
---
+++ index1.js
@@ -0,0 +1,5 @@
+const { x: a, y : b, z : c} = voxel; // a = 3.6, b = 7.4, c = 6.54
+
+//get the field of object x, assign to a.
+//get the field of object y, assign to b.
+//get the field of object z, assign to c.
